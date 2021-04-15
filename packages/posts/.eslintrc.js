ced625efb6369126ba1e340a6bd6f300ae74cc5a
nhat.tran <nhattq.coding@gmail.com>
2021-04-15 23:24:34 +0700

    :tada: init blog

---
+++ packages/posts/.eslintrc.js
@@ -0,0 +1,6 @@
+// // Append 'tsx' to Airbnb 'react/jsx-filename-extension' rule
+//     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+//     'react/jsx-filename-extension': 'off',
+//     // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+//     'react/destructuring-assignment': 'off',
+//     // Use function hoisting to improve code readability
