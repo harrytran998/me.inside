abed23e3075f3b0e06e50ef91b6ddde721885c12
nhat.tran <nhattq.coding@gmail.com>
2021-05-31 00:02:08 +0700

    main refactor: move to yarn 3 instead pnpm

--- creation-myths/zeus/.vitepress/config.js
+++ creation-myths/zeus/.vitepress/config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  title: 'Harry Trần',
+  title: "Harry Trần",
   head: [
-    ['link', { rel: 'icon', href: '/icon.svg' }],
+    ["link", { rel: "icon", href: "/icon.svg" }],
     [
-      'link',
+      "link",
       {
-        rel: 'stylesheet',
-        href: '//fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Dosis:wght@300;400;500;600;700&display=swap',
+        rel: "stylesheet",
+        href: "//fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Dosis:wght@300;400;500;600;700&display=swap",
       },
     ],
     [
-      'link',
+      "link",
       {
-        rel: 'alternative',
-        type: 'application/rss+xml',
-        href: '/feed.rss',
-        title: 'RSS Feed for harlanzw.com',
+        rel: "alternative",
+        type: "application/rss+xml",
+        href: "/feed.rss",
+        title: "RSS Feed for harlanzw.com",
       },
     ],
   ],
@@ -23,9 +23,9 @@ module.exports = {
     "Hey 👋 I'm building Laravel & Vue projects and would like to share my journey with you.",
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'About', link: '/about/' },
-      { text: 'Contact', link: '/contact/' },
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about/" },
+      { text: "Contact", link: "/contact/" },
     ],
   },
-}
+};
